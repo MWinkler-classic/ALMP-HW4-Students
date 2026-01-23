6423c19467947da9e06010e2363f179847cf7647
mwi <matan.wink@gmail.com>
2026-01-23 18:46:32 +0200

    Finish work for today. TODO: debug find_path not setting self parameters properly

--- RRTTree.py
+++ RRTTree.py
@@ -1,82 +1,170 @@
 import operator
-import numpy
+import numpy as np
 
 class RRTTree(object):
-    
-    def __init__(self, bb):
-        self.bb = bb
-        self.vertices = {}
-        self.edges = dict()
 
-    def GetRootID(self):
+    def __init__(self, bb, task="mp"):
+
+        self.bb = bb
+        self.task = task
+        self.vertices = {}
+        self.edges = {}
+        self.reverse_edges = {}
+
+        # inspecion planning properties
+        if self.task == "ip":
+            self.max_coverage = 0
+            self.max_coverage_id = 0
+
+    def get_root_id(self):
         '''
         Returns the ID of the root in the tree.
         '''
         return 0
 
-    def GetNearestVertex(self, config):
-        '''
-        Returns the nearest state ID in the tree.
-        @param config Sampled configuration.
-        '''
-        dists = [self.bb.edge_cost(config, self.vertices[v].state) for v in self.vertices]
-        # for v in self.vertices:
-        #     v_node = self.vertices[v]
-        #     dists.append(self.bb.edge_cost(config, v_node.state))
-
-        vid, vdist = min(enumerate(dists), key=operator.itemgetter(1))
-
-        return vid, self.vertices[vid]
-
-    def GetKNN(self, config, k):
-        '''
-        Return k-nearest neighbors
-        @param config Sampled configuration.
-        @param k Number of nearest neighbors to retrieve.
-        '''
-        # dists = []
-        dists = [self.bb.edge_cost(config, self.vertices[vid].state) for vid in self.vertices]
-        # for id in self.vertices:
-        #     dists.append(self.bb.edge_cost(config, self.vertices[id].state))
-
-        dists = numpy.array(dists)
-        knnIDs = numpy.argpartition(dists, k)[:k]
-        # knnDists = [dists[i] for i in knnIDs]
-
-        return knnIDs, None #[self.vertices[vid] for vid in knnIDs]
-
-    def AddVertex(self, config):
+    def add_vertex(self, config, inspected_points=None):
         '''
         Add a state to the tree.
-        @param real_conf: the real coords of the point in the hypertorus
         @param config Configuration to add to the tree.
         '''
         vid = len(self.vertices)
-        # self.vertices.append(config)
-        # self.vertices[vid] = RRTVertex(state=config)#, real_state=real_conf)
-        self.vertices[vid] = RRTVertex(state=config)#, real_state=real_conf)
+        self.vertices[vid] = RRTVertex(config=config, inspected_points=inspected_points)
+
+        # check if vertex has the highest coverage so far, and replace if so
+        if self.task == "ip":
+            v_coverage = self.bb.compute_coverage(inspected_points=inspected_points)
+            if v_coverage > self.max_coverage:
+                self.max_coverage = v_coverage
+                self.max_coverage_id = vid
+
         return vid
 
-    def AddEdge(self, sid, eid, edge_cost):
+    def add_edge(self, sid, eid, edge_cost=0):
         '''
         Adds an edge in the tree.
-        @param sid start state ID
+        @param sid stvart state ID
         @param eid end state ID
         '''
         self.edges[eid] = sid
+        if sid not in self.reverse_edges:
+            self.reverse_edges[sid] = []
+        self.reverse_edges[sid].append(eid)
         self.vertices[eid].set_cost(cost=self.vertices[sid].cost + edge_cost)
-        self.vertices[sid].children.add(self.vertices[eid])
 
+    def remove_edge(self, sid, eid):
+        self.edges.pop(eid, None)
+        self.reverse_edges[sid].remove(eid)
 
+    def is_goal_exists(self, config):
+        '''
+        Check if goal exists.
+        @param config Configuration to check if exists.
+        '''
+        goal_idx = self.get_idx_for_config(config=config)
+        if goal_idx is not None:
+            return True
+        return False
+
+    def get_vertex_for_config(self, config):
+        '''
+        Search for the vertex with the given config and return it if exists
+        @param config Configuration to check if exists.
+        '''
+        v_idx = self.get_idx_for_config(config=config)
+        if v_idx is not None:
+            return self.vertices[v_idx]
+        return None
+
+    def get_idx_for_config(self, config):
+        '''
+        Search for the vertex with the given config and return the index if exists
+        @param config Configuration to check if exists.
+        '''
+        valid_idxs = [v_idx for v_idx, v in self.vertices.items() if (v.config == config).all()]
+        if len(valid_idxs) > 0:
+            return valid_idxs[0]
+        return None
+
+    def get_nearest_config(self, config):
+        '''
+        Find the nearest vertex for the given config and returns its state index and configuration
+        @param config Sampled configuration.
+        '''
+        # compute distances from all vertices
+        dists = []
+        for _, vertex in self.vertices.items():
+            dists.append(self.bb.compute_distance(config, vertex.config))
+
+        # retrieve the id of the nearest vertex
+        vid, _ = min(enumerate(dists), key=operator.itemgetter(1))
+
+        return vid, self.vertices[vid].config
+
+    def get_edges_as_states(self):
+        '''
+        Return the edges in the tree as a list of pairs of states (positions)
+        '''
+
+        return [[self.vertices[val].config,self.vertices[key].config] for (key, val) in self.edges.items()]
+
+    def get_k_nearest_neighbors(self, config, k):
+        '''
+        Return k-nearest neighbors
+        @param state Sampled state.
+        @param k Number of nearest neighbors to retrieve.
+        '''
+        dists = []
+        for _, vertex in self.vertices.items():
+            dists.append(self.bb.compute_distance(config, vertex.config))
+
+        dists = np.array(dists)
+        knn_ids = np.argpartition(dists, k)[:k]
+        #knn_dists = [dists[i] for i in knn_ids]
+        knn_ids = np.argpartition(dists, k)[:k]
+        return knn_ids.tolist(), [self.vertices[vid].config for vid in knn_ids]
+
+    # New helpers to access stored costs
+    def get_cost(self, vid):
+        '''
+        Return the accumulated cost stored at vertex id vid.
+        '''
+        return self.vertices[vid].cost
+
+    def get_cost_for_config(self, config):
+        '''
+        Return cost for a vertex with given config (if exists), else None.
+        '''
+        v_idx = self.get_idx_for_config(config=config)
+        if v_idx is not None:
+            return self.vertices[v_idx].cost
+        return None
+
+    def update_subtree_cost_recursive(self, parent_id):
+        """
+        Update costs of all descendants of parent_id (parent cost assumed already correct).
+        Works with vertex ids.
+        """
+        for child_id in self.reverse_edges.get(parent_id, []):
+            edge_cost = self.bb.compute_distance(self.vertices[parent_id].config,
+                                                 self.vertices[child_id].config)
+            self.vertices[child_id].set_cost(self.vertices[parent_id].cost + edge_cost)
+            self.update_subtree_cost_recursive(child_id)
+
+    def update_subtree(self, v_id, new_parent_id, edge_cost):
+        """
+        Reparent vertex v_id to new_parent_id and update costs for v_id subtree.
+        """
+        old_parent = self.edges[v_id]
+        self.remove_edge(old_parent, v_id)
+        self.add_edge(new_parent_id, v_id, edge_cost=edge_cost)
+        self.update_subtree_cost_recursive(v_id)
 
 class RRTVertex(object):
 
-    def __init__(self, state, cost=0):
-
-        self.state = state
-        # self.real_state = real_state
+    def __init__(self, config, cost=0, inspected_points=None):
+        self.config = config
         self.cost = cost
-        self.children = set()
+        self.inspected_points = inspected_points
 
     def set_cost(self, cost):
         '''
